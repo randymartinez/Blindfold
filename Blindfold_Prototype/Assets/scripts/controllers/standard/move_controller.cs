6d8bd25432733e1bc4c7e2289eba3bade46b63b4
Marcus Williams <marcusw633@gmail.com>
2016-12-07 14:49:26 -0500

    Added standard first person controller and basic physics based movement

---
+++ Blindfold_Prototype/Assets/scripts/controllers/standard/move_controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_controller : MonoBehaviour {
+	
+	private Rigidbody rb;
+	public float moveSpeed;
+
+	public bool grounded = false;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+		if (col.collider.tag == "Ground")
+		{
+			grounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.collider.tag == "Ground")
+		{
+			grounded = false;
+		}
+	}
+
+	//FixedUpdate is called once per physics step
+	void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.W) || (Input.GetKey(KeyCode.UpArrow)))
+		{
+			rb.AddForce(transform.forward * moveSpeed);
+		}
+
+		if (Input.GetKey(KeyCode.S) || (Input.GetKey(KeyCode.DownArrow)))
+		{
+			rb.AddForce(-transform.forward * moveSpeed);
+		}
+	}
+}
